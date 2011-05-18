bb5d7016f681314ce44c67b2187e83ad724271dc
John Goodstadt <john@goodstadt.me.uk>
2011-05-18 11:10:49 +0100

    update

--- MidiNotes/midi.h
+++ MidiNotes/midi.h
@@ -4,6 +4,8 @@
 //
 //  Created by john goodstadt on 05/05/2011.
 //  Copyright 2011 John Goodstadt. All rights reserved.
+//  Use this file in your own projects as you see fit.
+//  Please email me at john@goodstadt.me.uk for any problems, fixes, addition or thanks.
 //
 
 #import <Foundation/Foundation.h>
