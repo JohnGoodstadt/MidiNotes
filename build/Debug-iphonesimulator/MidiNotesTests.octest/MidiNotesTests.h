53c080ab7433507c2162d03bda94973935d98012
John Goodstadt <john@goodstadt.me.uk>
2011-05-18 10:46:57 +0100

    Project Commit

---
+++ build/Debug-iphonesimulator/MidiNotesTests.octest/MidiNotesTests.h
@@ -0,0 +1,17 @@
+//
+//  MidiNotesTests.h
+//  MidiNotesTests
+//
+//  Created by john goodstadt on 18/05/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "midi.h"
+
+@interface MidiNotesTests : SenTestCase {
+@private
+    
+}
+
+@end
